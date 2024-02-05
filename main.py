c335881b8f524526063d3d0761668e8e9e7547e1
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-02-05 21:01:14 +0100

    start cleaning the data

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 # Loading the data
 data = pd.read_csv("cancer.csv")
@@ -10,3 +12,11 @@ print(data.info()) # succinct summary
 print(data.describe())
 
 # Cleaning the data
+sns.heatmap(data.isnull()) # visualizing missing data
+# plt.show()
+data.drop(["Unnamed: 32", "id"], axis=1, inplace=True)
+print(data.head())
+
+# convert 'M' and 'B' to 1 and 0 respectively
+data.diagnosis = [1 if value == "M" else 0 for value in data.diagnosis]
+print(data.head())
