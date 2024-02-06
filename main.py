32ce9a2b5886cf12db29988a00f5a59a70c62c4e
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-02-06 19:16:41 +0100

    clean the data and divide it into features and target variables

--- main.py
+++ main.py
@@ -20,3 +20,14 @@ print(data.head())
 # convert 'M' and 'B' to 1 and 0 respectively
 data.diagnosis = [1 if value == "M" else 0 for value in data.diagnosis]
 print(data.head())
+# to convert integers into a category
+data['diagnosis'] = data['diagnosis'].astype("category", copy=False)
+data['diagnosis'].value_counts().plot(kind="bar")
+# plt.show()
+
+# dividing the data into features and target variables
+y = data["diagnosis"] # our target variables
+X = data.drop(["diagnosis"], axis=1)
+
+print(y)
+print(X)
